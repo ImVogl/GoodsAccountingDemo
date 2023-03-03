3a940a4738bdbf4b14301c5521ca4c7090a0ff78
Роман Кармацкий <Knispel.Kurt@gmail.com>
2023-03-03 17:54:49 +0300

    Backend: Added JWT section

---
+++ Backend/Source/GoodsAccounting/Services/ConfigurationOptionSetup.cs
@@ -0,0 +1,30 @@
+﻿using GoodsAccounting.Model.Config;
+using Microsoft.Extensions.Options;
+
+namespace GoodsAccounting.Services;
+
+/// <summary>
+/// Configuration setup proxy.
+/// </summary>
+public class ConfigurationOptionSetup : IConfigureOptions<JwtSection>
+{
+    /// <summary>
+    /// Instance of <see cref="IConfiguration"/>.
+    /// </summary>
+    private readonly IConfiguration _configuration;
+
+    /// <summary>
+    /// Create new instance of <see cref="ConfigurationOptionSetup"/>.
+    /// </summary>
+    /// <param name="configuration">Instance of <see cref="IConfiguration"/>.</param>
+    public ConfigurationOptionSetup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    /// <inheritdoc />
+    public void Configure(JwtSection options)
+    {
+        _configuration.GetSection("JWT").Bind(options);
+    }
+}
