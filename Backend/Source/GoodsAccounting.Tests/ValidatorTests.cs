de2659b531ba13a3a05bf5413f2febf76aeb8548
Роман Кармацкий <Knispel.Kurt@gmail.com>
2023-03-03 13:34:20 +0300

    Added password validator

---
+++ Backend/Source/GoodsAccounting.Tests/ValidatorTests.cs
@@ -0,0 +1,57 @@
+﻿using GoodsAccounting.Services.Validator;
+using NUnit.Framework;
+
+namespace GoodsAccounting.Tests;
+
+public class ValidatorTests
+{
+    private static readonly IPasswordValidator PasswordValidator = new Validator();
+
+    [Test]
+    [Description("Test valid password")]
+    public void ValidPasswordTest()
+    {
+        const string validPassword = "Az!2.sssA";
+        Assert.True(PasswordValidator.Validate(validPassword));
+    }
+
+    [Test]
+    [Description("Password to short")]
+    public void PasswordTooShortTest()
+    {
+        const string invalidPassword = "Az!2.";
+        Assert.False(PasswordValidator.Validate(invalidPassword));
+    }
+
+    [Test]
+    [Description("Password doesn't contain special symbol")]
+    public void PasswordNoSpecialSymbol()
+    {
+        const string invalidPassword = "AzdG2sssA";
+        Assert.False(PasswordValidator.Validate(invalidPassword));
+    }
+
+    [Test]
+    [Description("Password doesn't contain digit character")]
+    public void PasswordNoDigitTest()
+    {
+        const string invalidPassword = "AzdG!sssA";
+        Assert.False(PasswordValidator.Validate(invalidPassword));
+    }
+
+    [Test]
+    [Description("Password doesn't contain upper case letter")]
+    public void PasswordNoUpperTest()
+    {
+        const string invalidPassword = "fz!2.sss.";
+        Assert.False(PasswordValidator.Validate(invalidPassword));
+    }
+
+    [Test]
+    [Description("Password doesn't contain lower case letter")]
+    public void PasswordNoLowerTest()
+    {
+        const string invalidPassword = "A2!2.2FGA";
+        Assert.False(PasswordValidator.Validate(invalidPassword));
+    }
+}
---
+++ Backend/Source/GoodsAccounting/Services/Validator/Validator.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GoodsAccounting.Services.Validator;
+
+/// <summary>
+/// Data validator.
+/// </summary>
+public class Validator : IPasswordValidator, IDtoValidator
+{
+    /// <inheritdoc />
+    public bool Validate(string value)
+    {
+        const int minPasswordLength = 8;
+        var regexWordUp = new Regex(@"[A-Z]");
+        var regexWordLow = new Regex(@"[a-z]");
+        var regexDigit = new Regex(@"\d");
+        var regexNotWord = new Regex(@"\W");
+        if (string.IsNullOrWhiteSpace(value))
+            return false;
+
+        if (value.Length < minPasswordLength)
+            return false;
+        
+        return regexWordUp.IsMatch(value) 
+               && regexWordLow.IsMatch(value) 
+               && regexDigit.IsMatch(value) 
+               && regexNotWord.IsMatch(value);
+    }
+}
