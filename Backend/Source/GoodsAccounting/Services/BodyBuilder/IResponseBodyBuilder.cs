945f057ba376a6d6d2743c360aab60b3e5eac38f
Роман Кармацкий <Knispel.Kurt@gmail.com>
2023-03-03 17:56:01 +0300

    Backend: Added request builder

---
+++ Backend/Source/GoodsAccounting/Services/BodyBuilder/IResponseBodyBuilder.cs
@@ -0,0 +1,26 @@
+﻿namespace GoodsAccounting.Services.BodyBuilder;
+
+/// <summary>
+/// Interface for request body builder service.
+/// </summary>
+public interface IResponseBodyBuilder
+{
+    /// <summary>
+    /// Build body for invalid DTO response.
+    /// </summary>
+    /// <returns>Response body.</returns>
+    Dictionary<string, string> InvalidDtoBuild();
+
+    /// <summary>
+    /// Build body for unknown exception response.
+    /// </summary>
+    /// <returns>Response body.</returns>
+    Dictionary<string, string> UnknownBuild();
+
+    /// <summary>
+    /// Build body for success response with token.
+    /// </summary>
+    /// <param name="token">JSON web token.</param>
+    /// <returns>Response body.</returns>
+    Dictionary<string, string> TokenBuild(string token);
+}
---
+++ Backend/Source/GoodsAccounting/Services/BodyBuilder/ResponseBodyBuilder.cs
@@ -0,0 +1,25 @@
+﻿namespace GoodsAccounting.Services.BodyBuilder;
+
+/// <summary>
+/// Request body builder service.
+/// </summary>
+public class ResponseBodyBuilder : IResponseBodyBuilder
+{
+    /// <inheritdoc />
+    public Dictionary<string, string> InvalidDtoBuild()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Dictionary<string, string> UnknownBuild()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public Dictionary<string, string> TokenBuild(string token)
+    {
+        return new Dictionary<string, string> { { "token", token } };
+    }
+}
