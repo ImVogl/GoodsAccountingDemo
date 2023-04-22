f9fe7161b2739344b049e37ab24e66e36bdfc788
Roman <knispel.kurt@gmail.com>
2023-04-22 21:44:21 +0300

    Mobile: added splash screen

---
+++ AndroidClient/app/src/main/java/com/goods_account/ui/splash/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.goods_account.ui.splash;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.goods_account.R;
+import com.goods_account.ui.login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            }
+        }, 4000);
+    }
+}
