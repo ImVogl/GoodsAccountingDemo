4c33a47ef26425a6c646b080d00771279f082d35
Roman <knispel.kurt@gmail.com>
2023-05-11 01:23:39 +0300

    Front: Added contact control component for account

--- Frontend/src/components/account/AccountPage.tsx
+++ Frontend/src/components/account/AccountPage.tsx
@@ -1,22 +1,23 @@
-import './Account.css';
 import classes from './Account.module.css';
 import { useAppSelector } from '../../common/redux/hooks';
 import { selectUserName, selectUserIsAdmin } from '../../common/redux/UserSlice';
 import { FC } from 'react';
 import { Container } from 'react-bootstrap';
-import WorkingArea from '../base/working/WorkingArea';
 import AddUser from './ManagerPanel/Add/AddUser';
 import RemoveUser from './ManagerPanel/Remove/RemoveUser';
 import PasswordChange from './AcountControl/Password/PasswordChangeControl';
 import ContactControl from './AcountControl/Contact/ContactControl';
+import LayoutBase from '../layouts/BaseLayout';
 
 const AccountPage: FC = () => {
     const displayedName = useAppSelector(selectUserName);
     const admin = useAppSelector(selectUserIsAdmin);
     return(
-        <WorkingArea>
-            <div className='account-page-root'>
-                <div className='account-header'>{admin ? "Менеджер" : "Продавец"} {displayedName}</div>
+        <LayoutBase>
+            <div className={classes.root}>
+                <div className={classes.accountHeader}>{displayedName}</div>
+                <div className={classes.accountRole}>{admin ? "Менеджер" : "Продавец"}</div>
+                <br />
                 <div className={classes.header}>Управление аккаунтом</div>
                 <ContactControl />
                 <PasswordChange />
@@ -24,7 +25,7 @@ const AccountPage: FC = () => {
                 {admin ? <AddUser /> : <Container />}
                 {admin ? <RemoveUser /> : <Container />}
             </div>
-        </WorkingArea>
+        </LayoutBase>
     );
 }
 
--- Frontend/src/components/account/AcountControl/Contact/ContactControl.tsx
+++ Frontend/src/components/account/AcountControl/Contact/ContactControl.tsx
@@ -1,10 +1,81 @@
-import { FC } from "react"
+import classes from './ContactControl.module.css'
+import { useAppSelector, useAppDispatch } from '../../../../common/redux/hooks';
+import { selectUserIdentifier } from '../../../../common/redux/UserSlice';
+import ApiClientWrapper from '../../../../common/utilites/ApiClientWrapper';
+import { FC, useState } from 'react';
+import { Form, Row } from 'react-bootstrap';
+import { badRequestProcessor } from '../../../../common/utilites/Common';
+import { CommonFloatingLabelInput } from '../../../UI/Form/FloatingLabelInput/CommonFloatingLabelInput';
+import { CommonButton } from '../../../UI/Button/CommonButton';
 
 const EMAIL_ID: string = 'email-contact-data';
 const TELEGRAM_ID: string = 'telegram-contact-data';
 
+// Обработка запроса на создание нового пользователя.
+const handleUpdateContactData = (identifier: number, client: ApiClientWrapper, form: HTMLFormElement): Promise<string> => {
+    let email:string = "";
+    let telegram: string = "";
+    for (let i = 0; i < form.length; i++){
+        if (form[i].localName !== "input"){
+            continue;
+        }
+
+        let element = (form[i] as HTMLInputElement);
+        if (element.id === EMAIL_ID){
+            email = element.value;
+        }
+
+        if (element.id === TELEGRAM_ID){
+            telegram = element.value;
+        }
+    };
+
+    return client.addNewUser(identifier, email, telegram, new Date())
+        .then(response => `Login: ${response.login}; Password: ${response.password}`)
+        .catch(exception => {
+            if (badRequestProcessor(exception)){
+                return "";
+            }
+    
+            console.error(exception);
+            return "";
+        });
+};
+
+// Компонента с контактными сведениями. 
 const ContactControl: FC = () => {
-    return <></>
+    const [submitting, setSubmitting] = useState(false);
+    const client = new ApiClientWrapper(useAppDispatch());
+    const identifier = useAppSelector(selectUserIdentifier);
+    return(
+        <Form 
+            className={classes.container}
+            onSubmit={event => {
+                event.preventDefault();
+                setSubmitting(true);
+                let form = event.target as HTMLFormElement;
+                handleUpdateContactData(identifier, client, form)
+                    .then(() => {
+                        form.reset();
+                        setSubmitting(false);
+                        })
+                    .catch(error => {
+                        console.error(error);
+                        form.reset();
+                        setSubmitting(false);
+                    });
+                    }
+                }
+            >
+            <Form.Group className={classes.container}>
+                <Form.Label className={classes.sublabel}>Изменить контактные данные</Form.Label>
+                <Row className={classes.row}>
+                    <Form.Group className={classes.column}><CommonFloatingLabelInput label='Email' type="email" id={EMAIL_ID} /></Form.Group>
+                    <Form.Group className={classes.column}><CommonFloatingLabelInput label='@telegram' type="text" id={TELEGRAM_ID} /></Form.Group>
+                    <CommonButton title='Сохранить' className={classes.column} type='submit' disabled={submitting} />
+                </Row>
+            </Form.Group>
+        </Form>);
 }
 
 export default ContactControl;
